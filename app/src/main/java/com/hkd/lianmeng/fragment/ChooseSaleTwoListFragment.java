684b88b728a5fcef29ef60261352af79367bfe38
gqf <gqf1995@qq.com>
2016-09-19 15:30:03 +0800

    修改sale头部选择，增加sale列表fragment，头部选择器fragment

---
+++ app/src/main/java/com/hkd/lianmeng/fragment/ChooseSaleTwoListFragment.java
@@ -0,0 +1,30 @@
+package com.hkd.lianmeng.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.johe.lianmengdemo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChooseSaleTwoListFragment extends Fragment {
+
+
+    public ChooseSaleTwoListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_sale_two_list, container, false);
+    }
+
+}
