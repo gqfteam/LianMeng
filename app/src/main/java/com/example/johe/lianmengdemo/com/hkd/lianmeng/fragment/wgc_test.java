a93087009b4b8f8342ce3a5ae21e318b9d4d191b
wangguangchun <740433720@qq.com>
2016-09-19 11:43:37 +0800

    wgc test2

--- app/src/main/java/com/example/johe/lianmengdemo/com/hkd/lianmeng/fragment/wgc_test.java
+++
@@ -1,8 +0,0 @@
-package com.example.johe.lianmengdemo.com.hkd.lianmeng.fragment;
-
-/**
- * Created by Administrator on 2016/9/19.
- */
-public class wgc_test {
-    //ssssssssssssss
-}
---
+++ app/src/main/java/com/hkd/lianmeng/constant/Constant.java
@@ -0,0 +1,10 @@
+package com.hkd.lianmeng.constant;
+
+import com.example.johe.lianmengdemo.R;
+
+/**
+ * Created by Administrator on 2016/9/18.
+ */
+public class Constant {
+    public static int[] FieldImage = {R.mipmap.myschool,R.mipmap.myschool, R.mipmap.myschool};
+}
