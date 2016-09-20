fb12d226d5c21ec37ae5c900f7cddb29421f762f
DanielGod <851976722@qq.com>
2016-09-20 09:00:33 +0800

    wjd调整MeFragment和MyAdapter_me_listView的位置 在MainActivity里添加了mMeFragment

--- app/src/main/java/com/hkd/lianmeng/adapter/MyAdapter_me_listView.java
+++ app/src/main/java/com/hkd/lianmeng/adapter/MyAdapter_me_listView.java
@@ -1,4 +1,4 @@
-package com.example.johe.lianmengdemo;
+package com.hkd.lianmeng.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.johe.lianmengdemo.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- app/src/main/java/com/hkd/lianmeng/fragment/MeFragment.java
+++ app/src/main/java/com/hkd/lianmeng/fragment/MeFragment.java
@@ -1,12 +1,16 @@
-package com.example.johe.lianmengdemo;
+package com.hkd.lianmeng.fragment;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.example.johe.lianmengdemo.R;
+import com.hkd.lianmeng.adapter.MyAdapter_me_listView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
