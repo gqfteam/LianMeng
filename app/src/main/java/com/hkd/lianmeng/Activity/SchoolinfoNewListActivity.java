e5888ecba03759a8290d8001b22fd05fd1813bc4
wangguangchun <740433720@qq.com>
2016-09-23 10:12:17 +0800

    wgc commit

---
+++ app/src/main/java/com/hkd/lianmeng/Activity/SchoolinfoNewListActivity.java
@@ -0,0 +1,15 @@
+package com.hkd.lianmeng.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.johe.lianmengdemo.R;
+
+public class SchoolinfoNewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schoolinfo_new_list);
+    }
+}
