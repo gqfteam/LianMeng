342d890ca6667cd51c4a2e5a5c2bfc4b618eb44b
lyt <491439301@qq.com>
2016-09-23 11:34:20 +0800

    lyt 发起签到页面

--- app/src/main/java/com/hkd/lianmeng/Activity/InitiateSigninActivity.java
+++ app/src/main/java/com/hkd/lianmeng/Activity/InitiateSigninActivity.java
@@ -1,8 +1,11 @@
 package com.hkd.lianmeng.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.johe.lianmengdemo.R;
 import com.hkd.lianmeng.adapter.InitialSigninShowClassAdapter;
@@ -10,10 +13,12 @@ import com.hkd.lianmeng.adapter.InitialSigninShowClassAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class InitiateSigninActivity extends AppCompatActivity {
+public class InitiateSigninActivity extends AppCompatActivity implements View.OnClickListener {
     ArrayList<HashMap<String,String>> classList;
     InitialSigninShowClassAdapter showClassAdapter;
     ListView showclass_listview;
+    TextView back_textview,cancel_btn;
+
 
 
 
@@ -50,12 +55,44 @@ public class InitiateSigninActivity extends AppCompatActivity {
         init();
         showClassAdapter=new InitialSigninShowClassAdapter(this,classList);
         showclass_listview.setAdapter(showClassAdapter);
-
+        addListener();
     }
+
+    /**
+     * 初始化组件
+     */
     public void init(){
         showclass_listview= (ListView) findViewById(R.id.initialsignin_bottom_showclass_listview);
+        back_textview= (TextView) findViewById(R.id.initialsignin_top_back_textview);
+        cancel_btn= (TextView) findViewById(R.id.initialsignin_bottom_cancel_btn);
 
     }
 
+
+    /**
+     * 点击事件
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.initialsignin_bottom_cancel_btn:
+                finish();
+                break;
+
+            case R.id.initialsignin_top_back_textview:
+                finish();
+                break;
+        }
+
+    }
+
+    /**
+     * 给组件添加点击事件
+     */
+    public void addListener(){
+        back_textview.setOnClickListener(this);
+        cancel_btn.setOnClickListener(this);
+    }
 
 }
